415e4ff5d6e7cdfbb67af2b6a80484fd570012f1
Danilov Eugene <daniloveugene@yandex.ru>
2017-04-18 16:05:45 +0300

    Edit base entity.

--- src/main/java/ru/kolaer/permit/entity/BaseEntity.java
+++ src/main/java/ru/kolaer/permit/entity/BaseEntity.java
@@ -3,16 +3,14 @@ package ru.kolaer.permit.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by danilovey on 18.04.2017.
  */
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
